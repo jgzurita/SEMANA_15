81ee7df451b920443fbcf1fe42c4fa83af1639e5
jgzurita <jg.zuritam@uea.edu.ec>
2024-09-23 23:16:05 -0500

    Verificar si existe telefono y agregar de no ser el caso

--- Diccionario.py
+++ Diccionario.py
@@ -12,3 +12,6 @@ informacion_personal = {
 informacion_personal["ciudad"] = "tena"
 informacion_personal["profesion"] = "agricultor"
 
+# Verificar si existe telefono y agregar de no ser el caso
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "0987654321"
